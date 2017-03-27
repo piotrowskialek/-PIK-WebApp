7931518e937ee146cfbfc4bf97520608b0114fb8
piotrowskialek <Polskiehaslo123>
2017-03-27 08:40:06 +0200

    refactor

--- HelloWorld/src/edu/elka/helloworld/controllers/HelloWorldController.java
+++ HelloWorld/src/edu/elka/helloworld/controllers/HelloWorldController.java
@@ -1,13 +1,8 @@
-package edu.elka.helloworld.edu.elka.helloworld.controllers;
+package edu.elka.helloworld.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.logging.Logger;
 
 /**
  * Created by apiotro on 26.03.17.
@@ -20,7 +15,6 @@ public class HelloWorldController {
 
     @RequestMapping("/hello")
     public ModelAndView hello(){
-
         ModelAndView model = new ModelAndView("hello");
         model.addObject("msg",++counter);
 
