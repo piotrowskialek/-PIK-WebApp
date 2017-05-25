77ff0224253320b680afa70393168e4596eefa3c
Aleksander Piotrowski <apiotrowski@MacBook-Pro-Aleksander.local>
2017-05-25 13:20:36 +0200

    PW-6 cassandra dao

--- src/main/java/edu/elka/peakadvisor/PeakadvisorApplication.java
+++ src/main/java/edu/elka/peakadvisor/PeakadvisorApplication.java
@@ -1,16 +1,9 @@
 package edu.elka.peakadvisor;
 
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.Session;
-import com.datastax.driver.core.querybuilder.QueryBuilder;
-import com.datastax.driver.core.querybuilder.Select;
-import edu.elka.peakadvisor.model.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.cassandra.core.CassandraOperations;
-import org.springframework.data.cassandra.core.CassandraTemplate;
 
 @SpringBootApplication
 public class PeakadvisorApplication {
@@ -20,7 +13,10 @@ public class PeakadvisorApplication {
 	public static void main(String[] args) {
 		LOGGER.info("dupa");
 		SpringApplication.run(PeakadvisorApplication.class, args);
+
 	}
+
+
 
 
 }
--- src/main/java/edu/elka/peakadvisor/controllers/PeakadvisorController.java
+++ src/main/java/edu/elka/peakadvisor/controllers/PeakadvisorController.java
@@ -1,12 +1,8 @@
 package edu.elka.peakadvisor.controllers;
 
-
-import org.springframework.data.annotation.Id;
+import edu.elka.peakadvisor.model.CassandraDao;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.datastax.driver.core.*;
-
-import java.io.Serializable;
 
 
 /**
@@ -15,33 +11,10 @@ import java.io.Serializable;
 @RestController
 public class PeakadvisorController {
 
-    public PeakadvisorController(){
-//
-//            Cluster cluster = Cluster.builder().addContactPoints("localhost").build();
-//            Session session = cluster.connect("pierwszy");
-//
-//            CassandraOperations template = new CassandraTemplate(session);
-//
-//            Task first = template.insert(new Task("1","xD"));
-//
-//            Select selectStatement = QueryBuilder.select().from("task");
-//            selectStatement.where(QueryBuilder.eq("id", first.getId()));
-//
-//            System.out.println(template.queryForObject(selectStatement, Task.class).getId());
-//
-//            template.truncate("task");
-//
-//            session.close();
-//            cluster.close();
-
-
-    }
-
     @RequestMapping("/")
     public String hello(){
 
-        return "hello";
-
+        return CassandraDao.getInstance().readLatest("xD");
     }
 
 }
