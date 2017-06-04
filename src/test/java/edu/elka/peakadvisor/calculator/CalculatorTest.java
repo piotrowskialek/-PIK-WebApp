d65bf610b221eed35b8c452246cc97fae4876d42
sylwiabla <sylwia.blach404@gmail.com>
2017-06-04 17:05:54 +0200

    add test file with data

--- src/test/java/edu/elka/peakadvisor/calculator/CalculatorTest.java
+++ src/test/java/edu/elka/peakadvisor/calculator/CalculatorTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class CalculatorTest {
 
-    private Calculator calculator=new Calculator();
+    private Calculator calculator=new Calculator("test-dataset.arff");
 
     @Test
     void calculateProfit() {
