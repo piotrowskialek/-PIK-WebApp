d7553c7734f36ae143818539712274773bbb62a7
Bartłomiej Boczek <bartek1boczek@gmail.com>
2017-06-05 06:31:10 +0200

    Dates can be choosen , final interface. No updates with rest available due to wrong api.

--- src/main/webapp/js/components/ChartComponent.js
+++ src/main/webapp/js/components/ChartComponent.js
@@ -4,9 +4,11 @@
 import Chart from 'chart.js';
 import React from 'react';
 
+
+
 export default class ChartComponent extends React.Component {
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.data = {
             "currency": "ZWL",
             "times": {
@@ -32,42 +34,55 @@ export default class ChartComponent extends React.Component {
                 "68400": "22.0"
             }
         }
+
+
+        this.state = {currency : [],
+                        times: [],
+        values : []};
+
+
+        this.updateData = this.updateData.bind(this);
+        this.renderChart = this.renderChart.bind(this);
+
         this.values = [];
         this.timestamps = [];
         this.getValues();
+
     }
-    getValues(){
-        for (var key in this.data['times']){
-            console.log(key)
+
+    getValues() {
+        this.currency = this.data['currency'];
+        for (var key in this.data['times']) {
+            // console.log(key)
             this.timestamps.push(key);
             this.values.push(this.data['times'][key]);
         }
-        // alert(this.data['times']['0'])
-        // console.log(this.timestamps[0])
-        // this.renderChart();
-        // alert(this.values[2])
     }
-    // componentDidMount() {
-    //     // let xmlHttp = new XMLHttpRequest();
-    //     // xmlHttp.open("GET", 'http://localhost:8080/PIK-WebApp-0.0.1-SNAPSHOT/getValue?currency=ZWL&start=0&end=68400'); // false for synchronous request
-    //     // xmlHttp.send(null);
-    //     // console.log(xmlHttp.responseText);
-    //     //
-    //
-    //
-    // };
-    componentDidMount(){
+
+
+    updateData(){
+        let times = this.state.times;
+        console.log(times);
+        console.log(this.state.values);
+        console.log(this.state.currency);
+        // this.values = this.state.values;
+        this.currency = this.state.currency;
+        // this.timestamps = this.state.times;
+    }
+
+    renderChart(){
         let chartCanvas = this.refs.chart;
-        // alert(this.timestamps.length);
+
 
         let myChart = new Chart(chartCanvas, {type: 'line',
                 data: {
-                    // labels: [new Date(86400000).toDateString(), new Date(86400030).toDateString(), new Date(86400020).toDateString(), new Date(86400040).toDateString(),
-                    //     new Date(86400000).toDateString(), new Date(86400030).toDateString(), new Date(86400020).toDateString(), new Date(86400040).toDateString()],
+                    // labels: this.state.times,
                     labels: this.timestamps,
                     datasets: [{
-                        label: this.data['currency'],
+                        // label: this.state.currency,
+                        label: this.currency,
                         // data: [12, 20, 3, 5, 3, 4, 5, 6],
+                        // data: this.state.values,
                         data: this.values,
                         fill: false,
                         backgroundColor: [
@@ -117,14 +132,66 @@ export default class ChartComponent extends React.Component {
 
         this.setState({chart: myChart});
     }
+    componentDidMount(){
+        var rest, mime, client;
+
+        rest = require('rest'),
+            mime = require('rest/interceptor/mime');
+
+        client = rest.wrap(mime);
+        client({ path: 'http://localhost:8080/PIK-WebApp-0.0.1-SNAPSHOT/getValue?currency=ZWL&start=0&end=68400' }).then(response => {
+            console.log(response);
+            // var a = JSON.parse(response['entity']);
+
+
+            // this.setState({currency : a['currency']});
+            // for (let key in a['times']){
+            //    console.log(key)
+            // }
+
+        });
+        // this.updateData();
+        // console.log(this.state.currency);
+        // console.log(this.currency);
+
+
+    }
+
+    componentWillReceiveProps(){
+        var rest, mime, client;
+
+        rest = require('rest'),
+            mime = require('rest/interceptor/mime');
+
+        let start = this.props.startDate;
+        let end = this.props.endDate;
+        client = rest.wrap(mime);
+        client({ path: 'http://localhost:8080/PIK-WebApp-0.0.1-SNAPSHOT/getValue?currency=ZWL&start=' + start + '&end=' + end}).then(response => {
+            console.log(response);
+            // var a = JSON.parse(response['entity']);
+            //
+            //
+            // this.setState({currency : a['currency']});
+            // for (let key in a['times']){
+            //     console.log(key)
+            // }
+
+        });
+        this.renderChart();
+    }
+
+
+    handleChange(e){
+       const currency = e.target.value;
+       this.setState({currency})
+    }
+
     render() {
-
-        // this.renderChart();
-
         return (
             <div>
                 <canvas ref={'chart'} height={'400'} width={'600'}/>
             </div>
+
         )
     };
 }
--- src/main/webapp/js/components/Layout.js
+++ src/main/webapp/js/components/Layout.js
@@ -6,14 +6,37 @@ import React from "react"
 import Title from './Title'
 import ChartComponent from './ChartComponent'
 import Input from "./Input";
+import DatePicker from './DatePicker'
 
 export default class Layout extends React.Component {
-     render(){
+    constructor() {
+        super();
+        this.state = {
+            startDate: "",
+            endDate: ""
+        }
+        this.setStartDate = this.setStartDate.bind(this);
+        this.setEndDate = this.setEndDate.bind(this);
+
+    }
+
+    setStartDate(date) {
+        console.log("start" + date)
+        this.setState({startDate: date});
+    }
+
+    setEndDate(date) {
+        console.log("end" + date)
+        this.setState({endDate: date});
+        console.log(this.state.endDate);
+    }
+
+    render() {
         return (
             <div>
                 <Title word1="Peak" word2="Advisor"/>
-                <Input/>
-                <ChartComponent/>
+                <Input changeSDate={this.setStartDate.bind(this)} changeEDate={this.setEndDate.bind(this)}/>
+                <ChartComponent startDate={this.state.startDate} endDate={this.state.endDate}/>
             </div>
         );
     }
