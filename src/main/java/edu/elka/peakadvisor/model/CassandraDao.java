e4b38e318656417fbc959933213ddfec460b28ca
apiotrowski <piotrowskialek@icloud.com>
2017-06-08 02:48:33 +0200

    jenkins costam

--- src/main/java/edu/elka/peakadvisor/model/CassandraDao.java
+++ src/main/java/edu/elka/peakadvisor/model/CassandraDao.java
@@ -106,7 +106,7 @@ public class CassandraDao {
         }).collect(Collectors.toList());
 
         return result;
-        ggd
+        g
     }
 
     public CassandraClusterFactoryBean getCluster() {
