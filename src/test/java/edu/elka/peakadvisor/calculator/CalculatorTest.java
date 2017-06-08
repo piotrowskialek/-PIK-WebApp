31835135742edf8421ce1810d7d5440e405e8dea
sylwiabla <sylwia.blach404@gmail.com>
2017-06-08 15:38:17 +0200

    remove polynomial tests - not deterministic enough

--- src/test/java/edu/elka/peakadvisor/calculator/CalculatorTest.java
+++ src/test/java/edu/elka/peakadvisor/calculator/CalculatorTest.java
@@ -43,25 +43,4 @@ class CalculatorTest {
         });
         assertEquals("All class values are the same. At least two class values should be different", exception.getMessage());
     }
-
-    @Test
-    void predictRatesLinearPolynomialRegression3 () {
-        ArrayList<Rate> testRates = new ArrayList<>();
-        testRates.add(new Rate (0, 10));
-        testRates.add(new Rate (3600, 20));
-        List<Rate> result = calculator.predictRatesPolynomial(testRates, 3600, 7200, 3);
-        assertEquals(true, (10 < result.get(0).getPrice()) && (result.get(0).getPrice() < 60));
-    }
-
-    @Test
-    void predictRatesPolynomialRegression9 () {
-        ArrayList<Rate> rates = new ArrayList<>();
-        rates.add(new Rate (0, 7.4));
-        rates.add(new Rate (3600, 5.4));
-        rates.add(new Rate (7200, 8.9));
-        rates.add(new Rate (10800, 8.1));
-        rates.add(new Rate(14400, 7.5));
-        List<Rate> result = calculator.predictRatesPolynomial(rates, 14401, 21600, 9);
-        assertEquals(true, (5 < result.get(0).getPrice()) && (result.get(0).getPrice() < 20));
-    }
 }
